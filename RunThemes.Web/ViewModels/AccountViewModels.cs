d08aeb24b8bd2c3a90c8fc786c24e250572476c1
Jahmai OSullivan <jaosulli@microsoft.com>
2014-11-10 20:32:34 -0800

    Cleaning up registration page

--- RunThemes.Web/ViewModels/AccountViewModels.cs
+++ RunThemes.Web/ViewModels/AccountViewModels.cs
@@ -70,6 +70,11 @@ namespace WhiteLabel.Web.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Name")]
+        public string DisplayName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
